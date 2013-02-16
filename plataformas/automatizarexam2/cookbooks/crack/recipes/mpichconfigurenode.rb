9adee94995610b1819d874d94ae152ed79f68bcc
themonki <edgarandres29@gmail.com>
2013-02-16 10:05:39 -0500

    cambios

--- plataformas/automatizarexam2/cookbooks/crack/recipes/mpichconfigurenode.rb
+++ plataformas/automatizarexam2/cookbooks/crack/recipes/mpichconfigurenode.rb
@@ -2,9 +2,9 @@ include_recipe "crack::getipadd"
 
 #enviar los host a la maquina local
 execute "install package" do
-        user "condor"
-        group "admin"
-        cwd "/home/condor"
+        user "vagrant"
+        #group "admin"
+        cwd "/home/vagrant"
         command "expect send_host_local.exp"
         action :run
 end
