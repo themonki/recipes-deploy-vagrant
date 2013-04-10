ddba7af08669344d84f5dcc4f7b30c5459549646
themonki <edgarandres29@gmail.com>
2013-04-10 16:30:40 -0500

    quitanto prueba expect

--- trabajoGrado/pruebaExpect/cookbooks/test/recipes/installexpect.rb
+++
@@ -1,8 +0,0 @@
-#http://www.cyberciti.biz/tips/execute-commands-on-multiple-hosts-using-expect-tool-part-iii.html
-
-execute "install expect" do
-	user "root"
-	cwd "/tmp"
-	command "yum -install -y expect expectk"
-	action :run
-end
