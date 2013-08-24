2644bb265d659a38bef8a90e1437f91a8441d3ad
themonki <edgarandres29@gmail.com>
2013-08-24 12:34:09 -0500

    agregando script bash para generar released automaticamente

---
+++ trabajoGrado/package-release.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+NAME="globus"
+function print_help { 
+											printf '%s\n' "";
+											printf '%s\n' "package-realease: empaqueta el contenido necesario para la ejecución de las ";
+											printf '%s\n' "recetas, para ignorar ciertos archivos agregarlos al archivo .package.ignore"  ;
+											printf '%s\n' "" ;
+											printf '\t%s\n' "Opciones:" ;
+											printf '%s\n' "" ;
+											printf '\t\t%s\n' "-n	NAME indica el nombre del archvio .tar.gz resultante, " ;
+											printf '\t\t%s\n' "		por defecto globus." ; 
+											printf '\t\t%s\n' "-h	imprime esta ayuda."   ; 
+											printf '%s\n' "" ;
+										} 
+
+function print_error	{
+												printf '%s\n' "";
+												printf '%s\n' "Error: Parámetros incorrectos.";
+												print_help;
+											}
+
+while getopts ':n:h' option;
+do
+	case "$option"
+		in
+			n) NAME=${OPTARG};;
+			h) print_help; exit 0;;
+			*) print_error; exit 2;;
+	esac
+done
+
+	mv  ./machineglobus/cookbooks/confighost/attributes/default.rb ./machineglobus/cookbooks/confighost/attributes/default.rb.tmp ;
+	cat ./machineglobus/cookbooks/confighost/attributes/default-release.rb | sed 's/^#*//g' > ./machineglobus/cookbooks/confighost/attributes/default.rb ;
+	
+	rm -rf $NAME.tar.gz ;
+	tar czvf $NAME.tar.gz -X .package.ignore  ./* ;
+	
+	rm ./machineglobus/cookbooks/confighost/attributes/default.rb ;
+	mv ./machineglobus/cookbooks/confighost/attributes/default.rb.tmp ./machineglobus/cookbooks/confighost/attributes/default.rb ;
+	
+	printf '\n%s\n' "Release salvado en $NAME.tar.gz"
+	printf '%s\n' "";
+
+exit 0
+
+
