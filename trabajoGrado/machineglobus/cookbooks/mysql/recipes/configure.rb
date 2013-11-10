fa74e21a7327a1d6785d970d1b1b38496a738d68
themonki <edgarandres29@gmail.com>
2013-11-10 11:03:37 -0500

    agregnado soporte para plataforma a recetas de mysql y psql

--- trabajoGrado/machineglobus/cookbooks/mysql/recipes/configure.rb
+++ trabajoGrado/machineglobus/cookbooks/mysql/recipes/configure.rb
@@ -2,23 +2,33 @@
 # https://help.ubuntu.com/8.04/serverguide/C/mysql.html
 #
 #http://dev.antoinesolutions.com/mysql
-execute "sed" do
-	user "root"
-	cwd "/tmp"
-	command "sed -e 's/^bind.*/bind-address = 172.19.0.2/' /etc/my.cnf > /tmp/my.cnf"
-	action :run
+
+class Chef
+ 	include Extensions::Platform
 end
 
-execute "move my.cnf" do
-	user "root"
-	cwd "/tmp"
-	command "mv /tmp/my.cnf /etc/my.cnf"
-	action :run
-end
+if platform?("redhat", "centos", "fedora")
+	if Extensions::Platform.amd64?
 
-execute "restart-mysql" do
-	user "root"
-	cwd "/tmp"
-	command "/etc/init.d/mysqld restart"
-	action :run
+    execute "sed" do
+	    user "root"
+	    cwd "/tmp"
+	    command "sed -e 's/^bind.*/bind-address = 172.19.0.2/' /etc/my.cnf > /tmp/my.cnf"
+	    action :run
+    end
+
+    execute "move my.cnf" do
+	    user "root"
+	    cwd "/tmp"
+	    command "mv /tmp/my.cnf /etc/my.cnf"
+	    action :run
+    end
+
+    execute "restart-mysql" do
+	    user "root"
+	    cwd "/tmp"
+	    command "/etc/init.d/mysqld restart"
+	    action :run
+    end
+  end
 end
--- trabajoGrado/machineglobus/cookbooks/postgresql/recipes/configure.rb
+++ trabajoGrado/machineglobus/cookbooks/postgresql/recipes/configure.rb
@@ -2,82 +2,93 @@
 ##RECORDAR QUE ES NECESARIO ORGANIZAR LAS IPTABLES
 ##http://www.cyberciti.biz/tips/postgres-allow-remote-access-tcp-connection.html
 
-cookbook_file "/tmp/pass.sql" do
-	source "pass.sql"
-	owner "postgres"
+
+class Chef
+ 	include Extensions::Platform
 end
 
-cookbook_file "/tmp/changepasspsql.exp" do
-	source "changepasspsql.exp"
-	owner "root"
-end
+if platform?("redhat", "centos", "fedora")
+	if Extensions::Platform.amd64?
+	
+    cookbook_file "/tmp/pass.sql" do
+	    source "pass.sql"
+	    owner "postgres"
+    end
 
-execute "initdb psql" do
-	command "service postgresql initdb"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    cookbook_file "/tmp/changepasspsql.exp" do
+	    source "changepasspsql.exp"
+	    owner "root"
+    end
 
-execute "chkconfig postgresql" do
-	command "chkconfig postgresql on"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    execute "initdb psql" do
+	    command "service postgresql initdb"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-execute "start psql" do
-	command "service postgresql start"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    execute "chkconfig postgresql" do
+	    command "chkconfig postgresql on"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-execute "pass sql" do
-	command "su postgres -c 'psql -f /tmp/pass.sql'"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    execute "start psql" do
+	    command "service postgresql start"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-execute "pass psql" do
-	command "expect changepasspsql.exp -p dbpsglobus2013"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    execute "pass sql" do
+	    command "su postgres -c 'psql -f /tmp/pass.sql'"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-execute "backups pg_hba.conf" do
-	command "mv /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.old"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    execute "pass psql" do
+	    command "expect changepasspsql.exp -p dbpsglobus2013"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-execute "backups postgresql.conf" do
-	command "mv /var/lib/pgsql/data/postgresql.conf /var/lib/pgsql/data/postgresql.conf.old"
-	user "root"
-	cwd "/tmp"
-	action :run  
-end
+    execute "backups pg_hba.conf" do
+	    command "mv /var/lib/pgsql/data/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf.old"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-cookbook_file "/var/lib/pgsql/data/postgresql.conf" do
-	source "postgresql.conf"
-	owner "postgres"
-	mode 0600
-end
+    execute "backups postgresql.conf" do
+	    command "mv /var/lib/pgsql/data/postgresql.conf /var/lib/pgsql/data/postgresql.conf.old"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
 
-cookbook_file "/var/lib/pgsql/data/pg_hba.conf" do
-	source "pg_hba.conf"
-	owner "postgres"
-	mode 0600
-end
+    cookbook_file "/var/lib/pgsql/data/postgresql.conf" do
+	    source "postgresql.conf"
+	    owner "postgres"
+	    mode 0600
+    end
 
-execute "restart psql" do
-	command "service postgresql restart"
-	user "root"
-	cwd "/tmp"
-	action :run  
+    cookbook_file "/var/lib/pgsql/data/pg_hba.conf" do
+	    source "pg_hba.conf"
+	    owner "postgres"
+	    mode 0600
+    end
+
+    execute "restart psql" do
+	    command "service postgresql restart"
+	    user "root"
+	    cwd "/tmp"
+	    action :run  
+    end
+
+  end
 end
 
 
